1d88ac041025e97b68c00eead50583c5dca5550b
20IT121 <20IT121@charusat.edu.in>
2022-09-19 14:13:03 +0530

    Added Edit/Delete Buttons to Students Listing.

--- src/component/StudentList.js
+++ src/component/StudentList.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Container, Table } from "react-bootstrap";
+import { ButtonGroup, Button } from "react-bootstrap";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudentList() {
   const [students, setStudents] = useState([]);
-
   useEffect(() => {
     getStudents();
   }, []);
-
   let getStudents = () => {
     axios
       .get("http://localhost:8080/listStudents")
@@ -19,7 +19,6 @@ export default function StudentList() {
   return (
     <div className="my-3">
       <Container>
-        <Card.Header><h3>Students List</h3></Card.Header>
         <Card.Header>
           <h3>Students List</h3>
         </Card.Header>
@@ -30,23 +29,10 @@ export default function StudentList() {
                 <th>Student Id</th>
                 <th>Student Name</th>
                 <th>Student Address</th>
+                <th>Edit/Delete</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-              </tr>
               {students.length === 0 ? (
                 <tr>
                   <td colSpan={3}>{students.length} Studnets Available!!!</td>
@@ -57,6 +43,12 @@ export default function StudentList() {
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
                 </tr>
                 )
               )}
