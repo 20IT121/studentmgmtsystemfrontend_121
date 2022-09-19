be73e426a8b592061a61aec16456e56a0ea043b5
20IT121 <20IT121@charusat.edu.in>
2022-09-19 22:39:45 +0530

    Added filter to refresh the table.

--- src/component/StudentList.js
+++ src/component/StudentList.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
+
 export default function StudentList() {
   const [students, setStudents] = useState([]);
   useEffect(() => {
