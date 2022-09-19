106405478963a3a2b4996555cf561d1491a72502
20IT121 <20IT121@charusat.edu.in>
2022-09-19 13:49:28 +0530

    Created Initial UI

--- src/component/Student.js
+++ src/component/Student.js
@@ -1,112 +1,34 @@
-import React, { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
+import React from "react";
+import { Container, Form, Card, Button } from "react-bootstrap";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export default function Student() {
-  const [id, setId] = useState();
-  const [name, setName] = useState();
-  const [address, setAddress] = useState();
-
-  const { studentId } = useParams(); //get the path parameter from the URL
-  const navigate = useNavigate();
-
-  const student = {
-    id: id,
-    name: name,
-    address: address,
-  };
-
-  useEffect(() => {
-    if (studentId !== null) {
-      axios
-        .get("http://localhost:8080/student/" + studentId)
-        .then((response) => {
-          setId(response.data.id);
-          setName(response.data.name);
-          setAddress(response.data.address);
-        })
-        .catch((error) => alert(error));
-    }
-  },[]);
-  let textchange = (event) => {
-    if (event.target.name === "id") {
-      setId(event.target.value);
-    } else if (event.target.name === "name") {
-      setName(event.target.value);
-    } else if (event.target.name === "address") {
-      setAddress(event.target.value);
-    }
-  };
-
-  let addstudent = (event) => {
-    if (studentId == null) {
-      event.preventDefault();
-      axios
-        .post("http://localhost:8080/student", student)
-        .then((response) => {
-          alert(response.data);
-        })
-        .catch((error) => alert(error));
-    } else {
-      axios
-        .put("http://localhost:8080/student/" + studentId, student)
-        .then((response) => alert(response.data))
-        .catch((error) => alert(error));
-    }
-    setId("");
-    setName("");
-    setAddress("");
-  };
   return (
-    <div className="my-4">
+    <div  className="my-3">
+    <Container>
       <Card>
-        <Card.Header>Add Student Information</Card.Header>
-        <Form onSubmit={addstudent}>
+        <Form>
+          <Card.Header>Add Student Information</Card.Header>
           <Card.Body>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Enter ID</Form.Label>
-              <Form.Control
-                name="id"
-                value={id}
-                type="text"
-                placeholder="Enter id"
-                onChange={textchange}
-              />
+              <Form.Label>Id</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
-
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Enter Name</Form.Label>
-              <Form.Control
-                name="name"
-                value={name}
-                type="text"
-                placeholder="Enter Name"
-                onChange={textchange}
-              />
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
-
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Enter Address</Form.Label>
-              <Form.Control
-                name="address"
-                value={address}
-                type="text"
-                placeholder="Enter Address"
-                onChange={textchange}
-              />
+              <Form.Label>Address</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
             </Form.Group>
           </Card.Body>
-
-          <Card.Footer>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Card.Footer>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
       </Card>
+    </Container>
     </div>
   );
 }
