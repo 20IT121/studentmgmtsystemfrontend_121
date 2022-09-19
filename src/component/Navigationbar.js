5e4470419e11e3342fb7137719e3acd818a9cd13
20IT121 <20IT121@charusat.edu.in>
2022-09-19 14:01:28 +0530

    Added Navigationbar

--- src/component/Navigationbar.js
+++ src/component/Navigationbar.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Navigationbar() {
+export default function NavigationBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Student Management System</Navbar.Brand>
+        <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/addStudent" className="nav-link">Add Student</Link>
-            <Link to="/viewStudent" className="nav-link">View Student</Link>
+            <Nav.Link href="#features">Add Student</Nav.Link>
+            <Nav.Link href="#pricing">View Students</Nav.Link>
+
           </Nav>
-          
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+    </div>
+  )
 }
